38d675028c3c614a41c30bf6229d30a57aed4b5d
Eniitan <enitanolawale0@gmail.com>
2026-06-07 20:17:58 +0100

    style format

--- main.go
+++ main.go
@@ -16,36 +16,35 @@ func main(){
   scanner := bufio.NewScanner(os.Stdin)
 
   if (scanner.Scan()){
-	old_path = scanner.Text()
+	  old_path = scanner.Text()
   }
 
   fmt.Printf("Enter the new name you want for the file: ")
   
   if (scanner.Scan()) {
-	new_name = scanner.Text()
+	  new_name = scanner.Text()
   }
 
   if err := scanner.Err(); err != nil {
-	fmt.Fprintf(os.Stderr, "%v\n", err)
-	os.Exit(1)
+	  fmt.Fprintf(os.Stderr, "%v\n", err)
+	  os.Exit(1)
   }
 
   if (old_path == "" || new_name == "") {
-	fmt.Fprintf(os.Stderr, "Error: File path and new name cannot be empty.\n")
-	os.Exit(1)
+	  fmt.Fprintf(os.Stderr, "Error: File path and new name cannot be empty.\n")
+	  os.Exit(1)
   }
 
   file, err := os.Stat(old_path)
   if (err != nil) {
-	fmt.Fprintf(os.Stderr, "%v\n", err)
-	os.Exit(1)
+	  fmt.Fprintf(os.Stderr, "%v\n", err)
+	  os.Exit(1)
   }
 
   if (file.IsDir()){
-	fmt.Fprintf(os.Stderr, "This is a folder, not a file, please enter a file path\n")
-	os.Exit(1)
+	  fmt.Fprintf(os.Stderr, "This is a folder, not a file, please enter a file path\n")
+	  os.Exit(1)
   }
-
 
   folder := filepath.Dir(old_path)
 
@@ -53,8 +52,8 @@ func main(){
   err = os.Rename(old_path, new_path)
 
   if (err != nil) {
-	fmt.Fprintf(os.Stderr, "%v\n", err)
-	os.Exit(1)
+	  fmt.Fprintf(os.Stderr, "%v\n", err)
+	  os.Exit(1)
   }
 
   fmt.Printf("File name successfully changed to %s\n", new_name)
