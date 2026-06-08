2c9db286d2f51581f68f795d298deed223e6b058
Eniitan <enitanolawale0@gmail.com>
2026-06-08 23:39:34 +0100

    setup a struct system to avoid duplicate operation

--- main.go
+++ main.go
@@ -8,10 +8,17 @@ import (
 	"strings"
 )
 
+type renameParams struct {
+  old_name string
+  new_name string
+  old_path string
+  new_path string
+}
+
 func main(){
   var folder string
-  var text_to_change string
   var text_to_replace string
+  var text_to_replace_with string
 
   fmt.Printf("Enter the path of the folder where you want to rename files: ")
 
@@ -24,13 +31,13 @@ func main(){
   fmt.Printf("Enter the subtext you want replace: ")
   
   if (scanner.Scan()) {
-	  text_to_change = scanner.Text()
+	  text_to_replace = scanner.Text()
   }
 
   fmt.Printf("Enter the text you want to replace it with: ")
 
   if (scanner.Scan()){
-    text_to_replace = scanner.Text()
+    text_to_replace_with = scanner.Text()
   }
 
   if err := scanner.Err(); err != nil {
@@ -38,7 +45,7 @@ func main(){
 	  os.Exit(1)
   }
 
-  if (text_to_change == "" || text_to_replace == "" || folder == "") {
+  if (text_to_replace == "" || text_to_replace_with == "" || folder == "") {
 	  fmt.Fprintf(os.Stderr, "\nError: Folder path and/or any of the rename parameters cannot be empty.\n")
 	  os.Exit(1)
   }
@@ -51,19 +58,34 @@ func main(){
 
   fmt.Printf("\n")
 
+  var details_of_files_to_rename []renameParams
+
   for _, file := range(files) {
     if (file.IsDir()){
       continue
     }
 
-    old_name := file.Name()
-    new_name := strings.ReplaceAll(old_name, text_to_change, text_to_replace)
+    oldName := file.Name()
+    newName := strings.ReplaceAll(oldName, text_to_replace, text_to_replace_with)
 
-    if(old_name == new_name) {
+    if(oldName == newName) {
       continue
     }
 
-    fmt.Printf("%s will be changed to %s\n", old_name, new_name)
+    details := renameParams{
+      old_name: oldName,
+      new_name: newName,
+      old_path: filepath.Join(folder, oldName),
+      new_path: filepath.Join(folder, newName),
+    }
+
+    details_of_files_to_rename = append(details_of_files_to_rename, details)
+    fmt.Printf("%s will be changed to %s\n", oldName, newName)
+  }
+
+  if (len(details_of_files_to_rename) < 1) {
+    fmt.Printf("No files matched the subtext '%s'\n", text_to_replace)
+    return
   }
 
   fmt.Printf("\nEnter 'Y' to proceed with the rename operation, enter anything else to terminate: ")
@@ -83,28 +105,15 @@ func main(){
   files_successfully_renamed := 0
   fmt.Printf("\n")
   
-  for _, file := range(files) {
-    if (file.IsDir()){
-      continue
-    }
-
-    old_name := file.Name()
-    new_name := strings.ReplaceAll(old_name, text_to_change, text_to_replace)
-
-    if (old_name == new_name){
-      continue
-    }
-
-    old_path := filepath.Join(folder, old_name)
-    new_path := filepath.Join(folder, new_name)
+  for _, file := range(details_of_files_to_rename) {
 
     // because if no error then the file already exist
-    if _, err = os.Stat(new_path); err == nil {
-      fmt.Printf("skipping %s because a file named %s already exists in this folder\n", old_name, new_name)
+    if _, err := os.Stat(file.new_path); err == nil {
+      fmt.Printf("skipping %s because a file named %s already exists in this folder\n", file.old_name, file.new_name)
       continue
     }
 
-    err := os.Rename(old_path, new_path)
+    err := os.Rename(file.old_path, file.new_path)
     if (err != nil) {
       fmt.Fprintf(os.Stderr, 
         "Error encountered while performing rename operation. %d files successfully renamed\nERROR DETAILS: %v\n", 
@@ -113,7 +122,7 @@ func main(){
       os.Exit(1)
     }
 
-    fmt.Printf("%s successfully changed to %s\n", old_name, new_name)
+    fmt.Printf("%s successfully changed to %s\n", file.old_name, file.new_name)
     files_successfully_renamed++
   }
 
