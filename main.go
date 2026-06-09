3fc7ab33e165c6425944f0f65cd7d26d0eb73f20
Enitan Akinfenwa <enitanolawale0@gmail.com>
2026-06-09 19:08:25 +0100

    explicitly check whether error is anything other than file not existing

--- main.go
+++ main.go
@@ -105,12 +105,19 @@ func perform_bulk_rename_op(file_details []renameParams) (int, error) {
   fmt.Printf("\n")
 
   for _, file := range(file_details) {
+    _, err := os.Stat(file.new_path)
     // because if no error then the file already exist
-    if _, err := os.Stat(file.new_path); err == nil {
-      return files_successfully_renamed, fmt.Errorf("skipping %s because a file named %s already exists in this folder", file.old_name, file.new_name)
+    if (err == nil) {
+      fmt.Printf("skipping %s because a file named %s already exists in this folder\n", file.old_name, file.new_name)
+      continue
     }
 
-    err := os.Rename(file.old_path, file.new_path)
+    //some other error
+    if (!os.IsNotExist(err)){
+      return files_successfully_renamed, fmt.Errorf("Error checking file %w", err)
+    }
+
+    err = os.Rename(file.old_path, file.new_path)
     if (err != nil) {
       return files_successfully_renamed, fmt.Errorf("Error encountered while performing rename operation. %d files successfully renamed\nERROR DETAILS: %w", 
         files_successfully_renamed, err)
